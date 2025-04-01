746b359f9dc742293d36cf6601dfffc2b8e9bd10
Dmitriy Korneev <korneevd123@gmail.com>
2025-04-01 14:24:38 +0300

    add 4 buttons and a status update message

--- src/bot/handlers/user_handlers.py
+++ src/bot/handlers/user_handlers.py
@@ -1,4 +1,4 @@
-from aiogram import types, Dispatcher, Router
+from aiogram import F, types, Dispatcher, Router
 from aiogram.filters import CommandStart
 
 from bot.keyboards.user_keyboards import get_main_kb
@@ -10,8 +10,16 @@ async def cmd_start(msg: types.Message) -> None:
     Args:
         msg (types.Message): msg object
     """
-    reply_text = (f'Привет, {msg.from_user.full_name}, как твои дела?\n'
-                  f'Я могу помочь тебе с поиском работы!')
+    reply_text = (f'Привет, {msg.from_user.full_name}, чем я могу тебе помочь?\n'
+                   '\n'
+                   'Я могу:\n'
+                   '\n'
+                   '    1. Предоставить тебе информацию о топе аниме по рейтингу\n'
+                   '    2. Предоставить тебе информацию о топе аниме по популярности\n'
+                   '    3. Предоставить тебе топ онгоингов по рейтингу\n'
+                   '    4. Порекомендовать тебе что-нибудь к просмотру\n'
+                   '\n'
+                   'Чего же желает твоя душа?')
 
     await msg.answer(
         text=reply_text,
@@ -19,10 +27,37 @@ async def cmd_start(msg: types.Message) -> None:
     )
 
 
+async def handle_main_menu(msg: types.Message) -> None:
+    """
+    Args:
+        msg: объект сообщения с кнопки главного меню
+
+    Returns:
+        None
+    """
+    await msg.answer("⏳ Обрабатываю ваш запрос...")
+
+
 def register_user_handlers(dp: Dispatcher) -> None:
-    """Register user handlers
+    """
+
+    Args:
+        dp:
+
+    Returns:
+
     """
     router = Router()
     router.message.register(cmd_start, CommandStart())
+
+    router.message.register(
+        handle_main_menu,
+        F.text.in_([
+            'Самые рейтинговые онгоинги',
+            'Топ по рейтингу',
+            'Топ по популярности',
+            'Получить рекомендации'
+        ])
+    )
 
     dp.include_router(router)
--- src/bot/keyboards/user_keyboards.py
+++ src/bot/keyboards/user_keyboards.py
@@ -1,11 +1,27 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-def get_main_kb() -> InlineKeyboardMarkup:
+def get_main_kb() -> ReplyKeyboardMarkup:
     """Get kb for main menu
     """
-    builder = InlineKeyboardBuilder()
-    builder.button(text='Найти работу', callback_data='find_job')
+    kb = [
+        [
+            KeyboardButton(text='Самые рейтинговые онгоинги')
+        ],
+        [
+            KeyboardButton(text='Топ по рейтингу'),
+            KeyboardButton(text='Топ по популярности'),
+        ],
+        [
+            KeyboardButton(text='Получить рекомендации')
+        ]
+    ]
 
-    return builder.as_markup()
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder='Выберите действие'
+    )
+
+    return keyboard
